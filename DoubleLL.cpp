7ea6cd5020d6dcd5694adc6687859b7ae1fef8ed
HabibaAtique <56055664+HabibaAtique@users.noreply.github.com>
2022-10-04 13:44:41 +0500

    Update DoubleLL.cpp

--- DoubleLL.cpp
+++ DoubleLL.cpp
@@ -1,3 +1,12 @@
+//  main.cpp
+//  BST
+//  Created by Hina Khalid  on 17/06/2020.
+//  Copyright © 2020 Hina Khalid . All rights reserved.
+//used by HabibaAtique
+//edited by Habiba Atique
+//DLL
+//Datastructures and Algorithms
+
 #include<iostream>
 using namespace std;
 class node 
